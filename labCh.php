b090d60b900e8e53372c2594d058a7c3b40ef531
hodzhakhov <85114829+hodzhakhov@users.noreply.github.com>
2023-02-07 10:30:44 +0300

    Update labCh.php

--- labCh.php
+++ labCh.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Лабиринты</title>
+    <title>Game</title>
   </head>
   <body>
     <?php
       @session_start();
     ?>
-    <p>Выбор лабиринта</p>
+    <p>Choose lab</p>
     <br>
     <table border="1">
-      <tr><td><a href="game.php">Лабиринт 1<?php @session_start(); $_SESSION["labC"] = 1 ?></a></td></tr>
-      <tr><td><a href="game.php">Лабиринт 2<?php @session_start(); $_SESSION["labC"] = 2 ?></a></td></tr>
-      <tr><td><a href="game.php">Лабиринт 3<?php @session_start(); $_SESSION["labC"] = 3 ?></a></td></tr>
+      <tr><td><a href="game.php">Lab 1<?php @session_start(); $_SESSION["labC"] = 1 ?></a></td></tr>
+      <tr><td><a href="game.php">Lab 2<?php @session_start(); $_SESSION["labC"] = 2 ?></a></td></tr>
+      <tr><td><a href="game.php">Lab 3<?php @session_start(); $_SESSION["labC"] = 3 ?></a></td></tr>
     </table>  
   </body>
 </html>
